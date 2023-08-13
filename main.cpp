9ecbb8f3b4787108aa23c2853ee2388b56554b17
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2023-08-13 09:45:42 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,16 +17,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     // Crear la ventana
     HWND hwnd = CreateWindowEx(
-        0,                              // Estilo adicional
-        CLASS_NAME,                     // Clase de ventana
-        L"Ventana con Diálogo",         // Título de la ventana
-        WS_OVERLAPPEDWINDOW,            // Estilo de la ventana
-        CW_USEDEFAULT, CW_USEDEFAULT,    // Posición de la ventana
-        800, 600,                       // Tamaño de la ventana
-        NULL,                           // Ventana padre
-        NULL,                           // Menú
-        hInstance,                      // Instancia de la aplicación
-        NULL                            // Datos de creación adicionales
+        0,                              // ---Estilo adicional
+        CLASS_NAME,                     // ---Clase de ventana
+        L"Ventana con Diálogo",         // ---Título de la ventana
+        WS_OVERLAPPEDWINDOW,            // ---Estilo de la ventana
+        CW_USEDEFAULT, CW_USEDEFAULT,   // ---Posición de la ventana
+        800, 600,                       // ---Tamaño de la ventana
+        NULL,                           // ---Ventana padre
+        NULL,                           // ---Menú
+        hInstance,                      // ---Instancia de la aplicación
+        NULL                            // ---Datos de creación adicionales
     );
 
     if (hwnd == NULL) {
